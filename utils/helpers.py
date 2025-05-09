b1bc2dca4f409255626de2d0cd0a3826150d5232
prabhat51 <88582253+prabhat51@users.noreply.github.com>
2025-05-09 18:37:30 +0530

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,9 @@
+def save_model(model, path):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    torch.save(model.state_dict(), path)
+
+def load_model(model, path, device):
+    model.load_state_dict(torch.load(path, map_location=device))
+    model.to(device)
+    model.eval()
+    return model
