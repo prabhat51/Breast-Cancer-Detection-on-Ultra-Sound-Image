ae55d5eeca40e2ed6322d6390cb50777b6d0abb9
prabhat51 <88582253+prabhat51@users.noreply.github.com>
2025-05-09 19:14:59 +0530

    Create transforms_segmentation.py

---
+++ segmentation/transforms_segmentation.py
@@ -0,0 +1,25 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+def get_train_transform(img_size=256):
+    """
+    Returns Albumentations Compose transform for training segmentation.
+    """
+    return A.Compose([
+        A.Resize(img_size, img_size),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.RandomRotate90(p=0.5),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),
+    ])
+
+def get_val_transform(img_size=256):
+    """
+    Returns Albumentations Compose transform for validation/testing segmentation.
+    """
+    return A.Compose([
+        A.Resize(img_size, img_size),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2(),
+    ])
